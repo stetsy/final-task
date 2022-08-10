284760e37319a568446894e377cd3b381683e1e3
stetsy <107214423+stetsy@users.noreply.github.com>
2022-08-10 23:56:56 +0300

    Update s3.tf

--- infrastructure/terafform/s3.tf
+++ infrastructure/terafform/s3.tf
@@ -10,24 +10,4 @@ module "s3_bucket" {
 
 }
 
-resource "aws_s3_bucket" "terraform_bucket" {
-      bucket = "prod-terraform-state-backend-114590890903"
-      acl    = "private"
-      region = "us-west-2"
-      versioning {
-          enabled = true
-      }
-    
-      tags = {
-          Name = "S3 Remote Terraform State Store"
-      }
-  }
 
-terraform {
-   backend "s3" {
-     bucket         = "prod-terraform-state-backend-114590890903"
-     key            = "terraform.tfstate"
-     region         = "us-west-2"
-
-  
-   }
